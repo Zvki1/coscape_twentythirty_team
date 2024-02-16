33bb3cae34a217039252a273d7634d8dbeee691c
Zvki1 <rzaki@hotmail.fr>
2024-02-16 05:46:39 +0100

    settuping the project

---
+++ landing_page/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+function App() {
+
+
+  return (
+    <Router>
+    <>
+   <h1 className=' text-red-600'>hello</h1>
+    </>
+    </Router>
+  )
+}
+
+export default App
